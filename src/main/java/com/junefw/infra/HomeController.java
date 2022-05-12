7840e14ee199f9dd705cb2b14dff68b61e0643fc
excitingmvr <excitingmvr@gmail.com>
2022-05-12 17:23:21 +0900

    asdf

--- src/main/java/com/junefw/infra/HomeController.java
+++ src/main/java/com/junefw/infra/HomeController.java
@@ -1,8 +1,15 @@
 package com.junefw.infra;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -46,6 +53,46 @@ public class HomeController {
 	public String zoomList() {
 		
 		return "test/zoomList";
+	}
+	
+	
+	@RequestMapping(value = "/test/publicCorona1List")
+	public String publicCorona1List() throws Exception {
+		
+		String apiUrl = "http://apis.data.go.kr/1471000/CovidDagnsRgntProdExprtStusService/getCovidDagnsRgntProdExprtStusInq?serviceKey=dNLcjyriV9IBD5djvIMsq16GYwW%2F8N%2FCtnCNvRj66yaLV9jXKhipDNCJFDcDzorgqnVsJsz5gmYoibNbAG0sdw%3D%3D&numOfRows=3&pageNo=1&type=json";
+		
+		//나중에 사용할 map을 선언해줍니다.
+		Map<String, Object> map = new HashMap<String, Object>();
+		 
+		URL url = new URL(apiUrl);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("GET");
+		conn.setRequestProperty("Content-type", "application/json");
+		System.out.println("Response code: " + conn.getResponseCode());
+		BufferedReader rd;
+		 
+		//getResponseCode가 200이상 300이하일때는 정상적으로 작동합니다.
+		if (conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+		    rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		} else {
+		    rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+		}
+		 
+		StringBuilder sb = new StringBuilder();
+		String line;
+		while ((line = rd.readLine()) != null) {
+		    sb.append(line);
+		}
+		rd.close();
+		conn.disconnect();
+		url =null;
+		//StringBuilder로 위에 파라미터 더 한값을 toString으로 불러옵니다.
+		//그리고 println으로 확인을 하면 xml형식이 나오게됩니다.
+		System.out.println(sb.toString());
+
+		
+		
+		return "test/publicCorona1List";
 	}
 	
 }
